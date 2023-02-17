3bb3ccb40dd61d0c122a8f6259f6e722f5eca03a
amarGhandour <30914748+amarGhandour@users.noreply.github.com>
2023-02-17 14:19:58 +0200

    [remove files] remove instructors and students tables.

--- Tables/dbo.Instructors.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Instructors] (
-  [id] [int] NOT NULL,
-  [name] [nchar](10) NULL,
-  [age] [int] NULL,
-  [Salary] [float] NULL,
-  [st_id] [nchar](10) NULL,
-  CONSTRAINT [PK_Instructors] PRIMARY KEY CLUSTERED ([id])
-)
-GO
--- Tables/dbo.Students.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Students] (
-  [id] [int] IDENTITY,
-  [name] [nvarchar](50) NOT NULL,
-  CONSTRAINT [PK_Students] PRIMARY KEY CLUSTERED ([id])
-)
-GO
